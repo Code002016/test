1979d0e5fb6afac579f07e2333bf99d3e4d12460
code016hiro <107974207+Code002016@users.noreply.github.com>
2023-10-19 12:03:43 +0700

    Update solve.py

--- Time-Controller/solve.py
+++ Time-Controller/solve.py
@@ -2,74 +2,73 @@ from pwn import *
 from ctypes import *
 import time
 
-context.log_level = 'debug'
+context.log_level = 'critical'
 context.arch = "amd64"
+
+e = context.binary = ELF('source')
+# r= e.process()
+# r= remote("34.126.117.161",2000)
+lib = e.libc
 
 proc = CDLL("/usr/lib/x86_64-linux-gnu/libc.so.6")
 timefunc = proc.time
 srand = proc.srand
 rand = proc.rand
-
-def rand_value():
-    srand(timefunc(0))
-    randnum = rand()
-    log.info("rand_value: %d" % randnum)
-    return randnum
-    
-def Time_Freeze():
-    return timefunc(0)+1
-
-e = context.binary = ELF('source')
-# r= e.process()
-r= remote("34.126.117.161",2000)
-lib = e.libc
-
-
+output = b""
 result  = 0xDEADBEEFDEADC0DE
+while(b"ISITDTU" not in output):
 
-def Elementary_Magic():
+    r= remote("34.126.117.161",2000)
+    
     r.recvuntil(b"================================Elementary_Magic================================\n")
-    time = int(r.recvline(),10)
-    rand = proc.rand
-    srand = proc.srand
-    srand(time)
+    
+    give = int(r.recvline(),10)
+    check = timefunc(0)
+    padd = give-check
+    
+    print("Give time: " +str(give))
+    print("Check time: " +str(check))
+    print("Padding: " +str(padd))
+
+    def rand_value():
+        srand(timefunc(0)+padd)
+        randnum = rand()
+        log.info("rand_value: %d" % randnum)
+        return randnum
+        
+    def Time_Freeze():
+        return timefunc(0)+padd
+
+    
+    srand(give)
     randnum = rand()
     print(randnum)
 
     r.sendafter(b"Pause time, enter to continue:",b"\x0a")
-    time    = Time_Freeze()
 
-    number  = c_longlong(result^randnum^time)
-    print("My Input_Number: ")
-    print(number.value)
-    pause()
+    number  = c_longlong(result^randnum^Time_Freeze())  #0xdeadbeefdeadc0de = num^rd^time1;
+    log.info("Input_Number: %d" % number.value)
+    #pause()
+    sleep(0.5)
     r.sendlineafter(b"Shout out the magic number sequence!\n", str(number.value))
     # r.interactive()
 
-def Advanced_Magic():
-    print("LV2:")
-    pause()
+    #pause()
+    sleep(0.5)
     r.sendafter(b"Scream your advanced magic!",b"a"*30 +b"::")
     r.recvuntil(b"::")
     urand_num = u64(r.recv(8).ljust(8,b"\x00"))
-    print("My urandnum: ")
-    print(hex(urand_num))
-
-    pause()
+    log.info("urand_num: %d" % urand_num)
+    #pause()
+    sleep(0.5)
     r.sendafter(b"Pause time, enter to continue:",b"\x0a")
-    srand_val= rand_value()
 
-    number  = c_longlong(result^urand_num^srand_val) 
-    print("My Input_Number: ")
-    print(number.value)
-    pause()
+    number  = c_longlong(result^urand_num^rand_value())  #result= (long long int)urand_num^srand_val^num;
+    log.info("Input_Number: %d" % number.value)
+    #pause()
+    sleep(0.5)
     r.sendlineafter(b"Shout out the magic number sequence!", str(number.value))
-    
-Elementary_Magic()
-Advanced_Magic()
-print(r.recvall())
+    output = r.recvall()
+
+print(output)
 r.interactive()
-
-
-
-    
